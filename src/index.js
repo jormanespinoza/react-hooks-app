56fe4c9177c60a8a35aee41f7b83cf50409820e1
Jorman <espinoza.dev@gmail.com>
2021-02-01 22:37:46 -0300

    useRef example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+import { FocusScreen } from './components/04-useRef/FocusScreen';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
 // import { SimpleForm } from './components/02-useEffect/SimpleForm';
 // import { CounterWithCustomHook } from './components/01-userState/CounterWithCustomHook';
@@ -8,6 +9,6 @@ import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHook
 // import { HookApp } from './HookApp';
 
 ReactDOM.render(
-  <MultipleCustomHooks />,
+  <FocusScreen />,
   document.getElementById('root')
 );
